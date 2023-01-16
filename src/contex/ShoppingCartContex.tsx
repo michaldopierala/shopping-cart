fc62f63843239e53a76eed34ab5baf015ef0a675
michaldopierala <michal6452@gmail.com>
2023-01-16 13:07:39 +0700

    all pages created; visualization of store page complited

---
+++ src/contex/ShoppingCartContex.tsx
@@ -0,0 +1,18 @@
+import { useContext, createContext, ReactNode } from "react"
+type ShoppingCartProviderProps={
+    children: ReactNode
+}
+const ShoppingCartContex = createContext({})
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContex)
+}
+
+
+export function ShoppingCartProvider({children}:ShoppingCartProviderProps){
+    return (
+        <ShoppingCartContex.Provider value={{}}>
+            {children}
+        </ShoppingCartContex.Provider>
+    )
+}
